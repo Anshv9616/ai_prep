950de2b29464fa45eb0c270933e0fe6db4013909
Anshv9616 <anshv9753@gmail.com>
2026-05-16 21:19:39 +0530

    fix

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -7,7 +7,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,pool_pre_ping=True, connect_args={"sslmode": "require"} )
+engine = create_engine( DATABASE_URL,
+   DATABASE_URL,
+    pool_pre_ping=True, 
+    pool_size=20,         
+    max_overflow=10,       
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
